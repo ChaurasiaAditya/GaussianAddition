a630a4f014c9ed0fe07298f9c9dd6d695e344497
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-03 18:33:36 +0530

    checked the method in main method

--- src/main/java/com/aditya/GaussianAddition.java
+++ src/main/java/com/aditya/GaussianAddition.java
@@ -5,12 +5,31 @@
  */
 package com.aditya;
 
+import java.util.Scanner;
+
 public class GaussianAddition {
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // prompt the user to enter the number
+        System.out.println("Enter a number: ");
+        double number = scanner.nextDouble();
+
+        // create an object to call the method
+        GaussianAddition gaussianAddition = new GaussianAddition();
+
+        // call the method and store the result in a variable
+        double Result = gaussianAddition.gaussianAddition(number);
+
+        // Display the result
+        System.out.println("The result is: " + Result);
+    }
 
     public double gaussianAddition(double number) {
         double a = 0;
         for (int i = 1; i <= number; i++) {
-            a +=i;
+            a += i;
         }
         return a;
     }
